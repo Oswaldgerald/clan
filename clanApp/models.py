45ca8146e73b948b661ffae78f0fdc38f230f454
Oswald Gerald <moswaldgerald@gmail.com>
2024-03-28 14:19:51 +0300

    Added features for the model Person and new model Residence

--- clanApp/models.py
+++ clanApp/models.py
@@ -1,14 +1,43 @@
 from django.db import models
 
 # Create your models here.
-class person(models.Model):
-     name = models.CharField(max_length=100)
+sex_choices = (
+        ('Male','Male'),
+        ('Female','Female'),	
+     )
+residence_choices = (
+     ('In Tanzania','In Tanzania'),
+    ('Outside Tanzania','Outside Tanzania'),
+)
+     
+class Person(models.Model):
+     
+     first_name = models.CharField(max_length=100)
+     middle_name = models.CharField(max_length=100)
+     surname = models.CharField(max_length=100)
+     nickname = models.CharField(max_length=100)
      date_of_birth = models.DateField((""), auto_now=False, auto_now_add=False)
      email = models.EmailField()
-     descendant_of = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+     spouse = models.CharField(max_length=100)
+     marital_status = models.CharField(max_length=100)
+     marriage_registered = models.BooleanField()
+     residence_status = models.CharField(choices = residence_choices,max_length=100)
+     country_of_residence = models.CharField(max_length=100)
+     address = models.ForeignKey('Residence', on_delete=models.CASCADE, null=True, blank=True)
      phone = models.CharField(max_length=10)
      address = models.TextField()
-     sex = models.CharField(max_length=1)
+     sex = models.CharField(choices = sex_choices,max_length=6)
+
      
      def __str__(self):
-         return self.name
+         return f"{self.first_name} {self.middle_name} {self.surname}"
+
+class Residence(models.Model):
+     
+     address = models.TextField()
+     region = models.CharField(max_length=100)
+     district = models.CharField(max_length=100)
+     
+     def __str__(self):
+         return f"{self.person} {self.address} {self.city} {self.state} {self.country} {self.zip_code} {self.start_date} {self.end_date}"
